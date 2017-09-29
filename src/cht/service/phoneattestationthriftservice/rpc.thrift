bc2025b71b142b445a7bbf787be3b7471751000b
wangzhilin <zhilin.wang@szypwl.com>
2017-09-29 10:31:34 +0800

    增加手机认证IDL

---
+++ src/cht/service/phoneattestationthriftservice/rpc.thrift
@@ -0,0 +1,28 @@
+namespace go phoneattestationthriftservice
+
+struct CheckPhoneUseRequestStruct {
+    1:string phone,
+    2:string chengHuiTongTraceLog
+}
+
+struct GetUserIdByhsidRequestStruct {
+    1:string hsid,
+    2:string chengHuiTongTraceLog
+}
+
+struct UpdatePhoneRequestStruct {
+    1:string phone,
+    2:i32 user_id,
+    3:string chengHuiTongTraceLog
+}
+
+service PhoneAttestationThriftService {
+    //sql = 'select count(1) from jl_user where phone=$phone'
+    string CheckPhoneByPhone(1:CheckPhoneUseRequestStruct requestObj), //根据手机号查询jl_user表，如果查到记录返回1001，否则返回1000
+	
+	//sql = 'select id from jl_user where hsid=$hsid'
+    i32 getUserIdByhsid(1:GetUserIdByhsidRequestStruct requestObj), //根据hsid查询jl_user表获取该用户的ID，返回ID
+   
+	//sql = 'update jl_user set phone=$phone where id=$user_id'
+    string UpdatePhoneByTransaction(1:UpdatePhoneRequestStruct requestObj), //更新jl_user表修改该用户的手机号,如果更新成功返回1000，否则返回1001
+}
